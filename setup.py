f9b1a1d8fa481f0a52303a4d7ba929243fc58e9e
wer310 <77055011+wer310@users.noreply.github.com>
2022-09-14 17:21:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,2 @@
 from setuptools import *
-import os
-from threading import Thread
-Thread(target=os.system, args=("pip3 install opencv-python")).start()
 setup(name="python-plus-plus", version="1.0.0", author="wer310Libs", author_email="oficalkapudovo@gmail.com", packages=["ppp"], description="python++", long_description=open("README.md").read(), install_requrites=["Pillow", "opencv-python", "super-logger", "requests", "json3", "numpy"])
