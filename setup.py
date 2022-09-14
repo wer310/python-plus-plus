bc183bf37742b73a2af76d575c6b2f8e4c3ce19b
wer310 <77055011+wer310@users.noreply.github.com>
2022-09-14 17:09:48 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,2 @@
+from setuptools import *
+setup(name="python-plus-plus", version="1.0.0", author="wer310Libs", author_email="oficalkapudovo@gmail.com", packages=["ppp"], description="python++", long_description=open("README.md").read(), install_requrites=["Pillow", "opencv-python", "super-logger", "requests", "json3", "numpy"])
